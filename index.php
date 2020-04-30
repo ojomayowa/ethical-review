d551254a47daf2ed568bbdf22a2bd22c5802face
MAYOWA <o.ojo2@rgu.ac.uk>
2020-04-30 18:24:02 +0100

    added welcome page

--- index.php
+++ index.php
@@ -1,1 +1,20 @@
-<?php
+
+<html>
+<head>
+    <title> Student - Ethical Approver </title>
+    <link rel="stylesheet" href="css/main.css">
+</head>
+
+<body>
+<main>
+    <section id="wrapper">
+        <h5> Experiment Ethical Approver Application </h5>
+        <ul>
+            <li><a href="student" target="_blank"> Student Section </a></li>
+            <li><a href="eao" target="_blank"> Experiment Approval Officer Section </a></li>
+            <li><a href="administrator" target="_blank"> Administrator Section </a></li>
+        </ul>
+    </section>
+</main>
+</body>
+</html>
